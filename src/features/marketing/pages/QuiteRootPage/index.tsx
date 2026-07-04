d6372874b5576e8bca4bee1fce6c43b36848e3db
wsuits6 <wsuits6@gmail.com>
2026-07-04 02:05:09 +0000

    enlarge QuiteRoot section logo on landing page, increase hero bottom padding on QuiteRoot page

--- src/features/marketing/pages/QuiteRootPage/index.tsx
+++ src/features/marketing/pages/QuiteRootPage/index.tsx
@@ -86,7 +86,7 @@ function Hero() {
   return (
     <div className="relative w-full min-h-[85svh] md:min-h-screen flex flex-col">
       <div className="relative z-30 w-full flex-1 mx-auto grid grid-cols-1 lg:grid-cols-2 text-left items-center md:h-full">
-        <div className="flex flex-col items-start justify-center px-6 sm:px-10 md:px-12 lg:pl-16 xl:pl-20 lg:pr-8 xl:pr-12 pt-20 sm:pt-28 lg:pt-24 pb-10 sm:pb-12 lg:pb-16 w-full h-full">
+        <div className="flex flex-col items-start justify-center px-6 sm:px-10 md:px-12 lg:pl-16 xl:pl-20 lg:pr-8 xl:pr-12 pt-20 sm:pt-28 lg:pt-24 pb-20 sm:pb-24 lg:pb-28 w-full h-full">
           <div className="flex flex-col items-start w-full space-y-6">
             <h1 className="text-5xl md:text-7xl lg:text-8xl font-black uppercase tracking-tight leading-[0.9]">
               QUITE <span className="text-accent">ROOT</span>
@@ -105,7 +105,7 @@ function Hero() {
             </div>
           </div>
         </div>
-        <div className="relative hidden lg:flex items-center justify-center w-full h-full pt-20 xl:pt-24">
+        <div className="relative hidden lg:flex items-center justify-center w-full h-full pt-20 xl:pt-24 pb-20 lg:pb-28">
           <div className="relative z-10 w-full h-full max-w-[80%] 2xl:max-w-[75%] flex items-center justify-center">
             <img
               src={quiteRootLogo}
