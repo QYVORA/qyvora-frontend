7b7af85d2058bf612309fba258217a4455b930f0
wsuits6 <wsuits6@gmail.com>
2026-07-05 11:33:42 +0000

    chore: update QuiteRoot logo assets + hero layout fixes

--- src/features/marketing/pages/QuiteRootPage/index.tsx
+++ src/features/marketing/pages/QuiteRootPage/index.tsx
@@ -86,8 +86,8 @@ function Hero() {
   return (
     <div className="relative w-full min-h-[85svh] md:min-h-screen flex flex-col">
       <div className="relative z-30 w-full flex-1 mx-auto grid grid-cols-1 lg:grid-cols-2 text-left items-center md:h-full">
-        <div className="flex flex-col items-start justify-center px-6 sm:px-10 md:px-12 lg:pl-16 xl:pl-20 lg:pr-8 xl:pr-12 pt-16 sm:pt-20 lg:pt-24 pb-14 sm:pb-16 lg:pb-16 w-full h-full">
-          <div className="flex flex-col items-start w-full space-y-6">
+        <div className="flex flex-col items-start justify-start px-6 sm:px-10 md:px-12 lg:pl-16 xl:pl-20 lg:pr-8 xl:pr-12 pt-20 sm:pt-24 lg:pt-28 pb-14 sm:pb-16 lg:pb-16 w-full h-full">
+          <div className="flex flex-col items-start w-full space-y-5">
             <h1 className="text-5xl md:text-7xl lg:text-8xl font-black uppercase tracking-tight leading-[0.9]">
               QUITE <span className="text-accent">ROOT</span>
             </h1>
@@ -105,12 +105,12 @@ function Hero() {
             </div>
           </div>
         </div>
-        <div className="relative hidden lg:flex items-center justify-center w-full h-full pt-20 xl:pt-24">
-          <div className="relative z-10 w-full h-full max-w-[80%] 2xl:max-w-[75%] flex items-center justify-center">
+        <div className="relative hidden lg:flex items-center justify-center w-full pt-20 xl:pt-24">
+          <div className="relative z-10 w-full max-w-[80%] 2xl:max-w-[75%] flex items-center justify-center">
             <img
               src={quiteRootLogo}
               alt="QuiteRoot"
-              className="w-full h-full object-contain"
+              className="w-full h-auto object-contain"
             />
           </div>
         </div>
