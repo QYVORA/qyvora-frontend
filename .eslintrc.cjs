f91a0097142e4beac89d5a2726e834fbfdac28f8
wsuits6 <wsuits6@gmail.com>
2026-07-07 09:23:53 +0000

    chore: allow empty catch blocks and fix unnecessary escapes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,5 +19,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'prefer-const': 'off',
+    'no-empty': ['error', { allowEmptyCatch: true }],
   },
 };
