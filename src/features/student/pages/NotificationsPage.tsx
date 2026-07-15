59dc760282c1d0f3409b217b8bc591e1269e9065
wsuits6 <wsuits6@gmail.com>
2026-07-15 01:06:41 +0000

    fix: consistent spacing between overview cards and content sections

--- src/features/student/pages/NotificationsPage.tsx
+++ src/features/student/pages/NotificationsPage.tsx
@@ -99,7 +99,7 @@ const Notifications: React.FC = () => {
           className="w-full flex-1 min-w-0 lg:h-full lg:overflow-y-auto lg:overscroll-contain scroll-hover"
           style={{ WebkitMaskImage: 'linear-gradient(to bottom, transparent 0px, black 24px)', maskImage: 'linear-gradient(to bottom, transparent 0px, black 24px)' }}
         >
-          <div className="px-2 sm:px-6 md:px-8 pb-16 lg:px-8 lg:py-6">
+          <div className="px-2 sm:px-6 md:px-8 pb-16 lg:px-8 lg:py-6 space-y-6">
 
             {/* Page header */}
             <LearningOverviewCard
