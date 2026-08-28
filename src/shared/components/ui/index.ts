4efb44370024a704d36a585f4556af8d7cd3507b
wsuits6 <wsuits6@gmail.com>
2026-08-28 01:57:20 +0000

    feat(components): implement canonical LearningCard and wire shared card primitives

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -1,6 +1,6 @@
 export { Dialog, DialogContent, DialogTrigger, DialogClose, ConfirmDialog } from './Dialog';
 export { BottomSheet, BottomSheetContent, BottomSheetClose } from './BottomSheet';
-export { CardBase, CardMedia } from './Card';
+export { CardBase, CardMedia, CardStat, LearningCard, DifficultyBadge } from './Card';
 export { Tooltip, TooltipProvider } from './Tooltip';
 export { default as StatCounter } from './StatCounter';
 export { default as SimpleHeading } from './SimpleHeading';
